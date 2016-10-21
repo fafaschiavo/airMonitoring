3b07ff914196f671d5cf4eec18b1120558f574b3
Fabricio Muche Schiavo <fafaschiavo@Fabricios-MacBook-Air.local>
2016-10-21 18:24:47 +0200

    teste3 added

--- python_receiver/receiver.py
+++ python_receiver/receiver.py
@@ -3,6 +3,9 @@ import csv
 
 print 'Hi there!'
 
+#Completar com o numero de amostras que estao sendo enviadas
+samble_amount = 3
+
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 line = ''
@@ -10,14 +13,13 @@ counter = 0
 with open('data_air_motherfucker.csv', 'wb') as csvfile:
 	while True:
 		raw_data = ser.readline()
-		# print raw_data.split('\n')[0]
 		if len(raw_data.split('\n')[0]) < 5:
 			processed_data = raw_data.split('\n')
 			final_data = processed_data[0].split('\r')[0]
 			line = line + final_data + ';'
 			counter = counter + 1
 		else:
-			if counter == 3:
+			if counter == samble_amount:
 				spamwriter = csv.writer(csvfile, delimiter=';')
 				data = []
 				data.append(line)
---
+++ teste2/teste2_1.ino
@@ -0,0 +1,32 @@
+int air_speed = A0;
+int co_concentration = A1;
+int co2_concentration = A2;
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int air_speed_value;
+int co_concentration_value;
+int co2_concentration_value;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Hello Computer");
+}
+
+void loop() {
+  // read the value from the sensor:
+  air_speed_value = analogRead(air_speed);
+  co_concentration_value = analogRead(co_concentration);
+  co2_concentration_value = analogRead(co2_concentration);
+  
+  Serial.println(air_speed_value);
+  Serial.println(co2_concentration_value);
+  Serial.println(co_concentration_value);
+  Serial.println("endpoint");
+
+  delay(300);
+}
+
+
+
